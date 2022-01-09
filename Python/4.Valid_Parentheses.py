f1c5e5f569ca2cf6deb1b0cf3f81a327154327d3
ShaanKohli <shaan.kohli@mail.mcgill.ca>
2022-01-08 23:27:20 -0500

    more problems

--- Python/4.Valid_Parentheses.py
+++ Python/4.Valid_Parentheses.py
@@ -1,2 +1,30 @@
-
-
+class Solution(object):
+    def isValid(self, s):
+        open_close_map = {
+        "(" : ")",
+        "[": "]",
+        "{" : "}
+        }
+      
+        para = []
+        
+        
+        if len(s)%2 != 0 or len(s) == 0:
+            return False
+        
+        for i in range(0, len(s)):
+            if s[i] in open_close_array.keys():
+                para = para.append(s[i])
+            
+            elif s[i] in open_close_array.values() and para:
+                if s[i] != open_close_map[para.pop()]:
+                    return False #closing was not correct
+            else:
+                return False #starts with closing
+        
+        return False #incomplete closing
+    
+    return True #all closed correctly
+        
+        
+        
