dcbe03a90ad9f822d91ccd1f453eb5e02671e4fa
Shaan K <60670209+ShaanKohli@users.noreply.github.com>
2022-01-23 23:40:50 +0000

    more changes

--- SQL/Asana.sql
+++ SQL/Asana.sql
@@ -16,17 +16,28 @@
 -- 2020-01-03 | 456        | 'premium' | 60      | 5
 
 -- How many accounts upgraded to a higher plan tier in the last month?
+---- CALL THIS the base_table
+SELECT 
+  account_id,
+  plan_tier,
+  CASE WHEN plan_tier = 'free' THEN 1 ELSE
+    (CASE WHEN plan_tier = 'premium' = 2 ELSE
+      (CASE WHEN plan_tier = 'business' = 3 ELSE
+        (CASE WHEN plan_tier = 'enterprise' = 4 ELSE NULL END) END) END) END as plan_rank
+  min(date) as min_date
+FROM customer_plan_history
+WHERE date >= current_date -30
+GROUP BY 1,2
 
 
 SELECT
   COUNT(DISTINCT x.account_id)
-  
 FROM 
   (
     SELECT 
       t1.account_id, 
-      CASE WHEN  t2.plan_tier > t1.plan_tier and t2.date - t1.date < 0 THEN 1 ELSE 0 END upgraded_flag
-    FROM customer_plan_history t1 
-    LEFT JOIN customer_plan_history t2 ON t1.account_id = t2.account_id
+      CASE WHEN  t2.plan_rank > t1.plan_rank and t2.min_date - t1._date > 0 THEN 1 ELSE 0 END upgraded_flag
+    FROM base_table t1 
+    LEFT JOIN base_table t2 ON t1.account_id = t2.account_id
   ) x
   WHERE x.upgraded_flag > 1
---
+++ SQL/FB Sample.sql
@@ -0,0 +1,12 @@
+-- An attendance log for every student in a school district attendance_events : 
+-- date | student_id | attendance
+
+-- A summary table with demographics for each student in the district all_ students : 
+-- student_id | school_id | grade_level | date_of_birth | hometown
+
+-- 1) What percent of students attend school on their birthday?
+
+SELECT
+FROM attendance_los
+
+-- 2) Which grade level had the largest drop in attendance between yesterday and today?
