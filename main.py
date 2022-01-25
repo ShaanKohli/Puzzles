a36a0432a51a61e06cdd066ba9296cd5da67ef0b
Shaan K <60670209+ShaanKohli@users.noreply.github.com>
2022-01-25 18:14:49 +0000

    more problems

--- main.py
+++ main.py
@@ -1,20 +1,8 @@
-#def reverseVowels(self, s: str) -> str:
-input_string = 'leetcode'
-vowel_string = ''
-index_string = []
-input_string = list(input_string)
+s = ["h","e","l","l","o"]
 
-for index, letter in enumerate(input_string):
-  if letter.lower() in ['a', 'e','i','o','u']:
-    index_string.append(index)
-    vowel_string = vowel_string +letter
-
-  ## reverse both 
-reverse_vowel_string = list(vowel_string[::-1])
-
-for i in range(0, len(input_string)):
-  input_string[index_string[i]] = vowel_string[i]
-
-input_string = "".join(input_string)
-
-print(input_string)
+traverse_length = len(s)//2
+        
+for i in range(0, traverse_length):
+  holding_pointer = s[i]
+  s[i] = s[len(s) -1 - i]
+  s[len(s) -1 - i] = holding_pointer
